d7a0caae1e207a04d8f44e2fc87b0919f12e060f
@costamatthew <mmateusdsilvacosta@outlook.com>
2021-09-17 22:02:17 -0300

    feat: add NextAuth - singIn and singOut with github auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps } from "next";
+//import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -38,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JaPg5IDI8IDXP1dHZN2WWPM");
 
   const product = {
@@ -53,5 +54,24 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve("price_1JaPg5IDI8IDXP1dHZN2WWPM");
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat("en-US", {
+//       style: "currency",
+//       currency: "USD",
+//     }).format(price.unit_amount / 100),
+//   };
+
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// };
